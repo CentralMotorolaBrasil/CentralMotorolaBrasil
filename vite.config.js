9a3b92a5ab46b8bfa4d859bec287baa53ba008b1
Luquinhas <lucmsilva651@gmail.com>
2026-04-11 02:10:05 -0300

    Adiciona o Terser como dependência e configura opções de minificação e warmup no Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,25 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
+  server: {
+    warmup: {
+      clientFiles: [
+        './src/views/HomeView.vue',
+        './src/data/devices/index.js',
+        './src/utils/i18n.js',
+      ],
+    },
+  },
 })
